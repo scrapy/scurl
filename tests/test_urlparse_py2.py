870aa30df84b63b48b7f127e06b963b7f76d0ba7
nctl144 <nctl144@gmail.com>
2018-07-06 13:18:12 -0500

    skip importing urlparse

--- tests/test_urlparse_py2.py
+++ tests/test_urlparse_py2.py
@@ -1,8 +1,9 @@
 from test import test_support
 import unittest
-import urlparse
 
 pytest.skip("Test file is under development", allow_module_level=True)
+
+import urlparse
 
 RFC1808_BASE = "http://a/b/c/d;p?q#f"
 RFC2396_BASE = "http://a/b/c/d;p?q"
